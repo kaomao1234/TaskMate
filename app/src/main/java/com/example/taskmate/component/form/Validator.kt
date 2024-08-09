35426833e52d00b7179c8c8c864f45a1297caa2d
Beginner-Project <boripats63@nu.ac.th>
2024-08-09 23:36:47 +0700

    feat: fix type.

--- app/src/main/java/com/example/taskmate/component/form/Validator.kt
+++ app/src/main/java/com/example/taskmate/component/form/Validator.kt
@@ -1,31 +1,31 @@
 package com.example.taskmate.component.form
 
-class Validator(private val value: String) {
+class Validator(private val value: Any) {
     private val errors: MutableList<String> = mutableListOf<String>()
 
     fun require(message: String = "This field is required"): Validator {
-        if (value.isEmpty()) {
+        if ((value as String).isEmpty()) {
             errors.add(message)
         }
         return this
     }
 
     fun minLength(minLength: Int, message: String = "Password must be at least $minLength characters"): Validator {
-        if (value.length < minLength) {
+        if ((value as String).length < minLength) {
             errors.add(message)
         }
         return this
     }
 
-    fun equalString(other:Any,message: String = "is equal"):Validator{
-        if(other.equals(value)){
+    fun equalString(other:String,message: String = "is equal"):Validator{
+        if(other.equals(value as String)){
             errors.add(message)
         }
         return this
     }
 
-    fun notEqualString(other:Any,message: String = "is not equal"):Validator{
-        if(!other.equals(value)){
+    fun notEqualString(other:String,message: String = "is not equal"):Validator{
+        if(!other.equals(value as String)){
             errors.add(message)
         }
         return this
@@ -33,14 +33,14 @@ class Validator(private val value: String) {
     }
 
     fun specialCharacter(message: String = "Must contain a special character"): Validator {
-        if (value.none { !it.isLetterOrDigit() }) {
+        if ((value as String).none { !it.isLetterOrDigit() }) {
             errors.add(message)
         }
         return this
     }
 
     fun emailForm(message: String = "Invalid email format"): Validator {
-        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(value).matches()) {
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(value as String).matches()) {
             errors.add(message)
         }
         return this
